11f9d48be0c381ae101ea17c4a470b6de1a08e0b
Matheus <matheusoliveira09222004@gmail.com>
2024-10-30 20:33:09 -0300

    Category and Created_By are now Class Based Views

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from blog.views import PostListView, page, post, created_by, category, tag, search
+from blog.views import (
+    PostListView, CreatedByListView, CategoryListView, page, post, tag, search
+)
 
 APP_NAME = 'blog'
 
@@ -7,8 +9,12 @@ urlpatterns = [
     path('', PostListView.as_view(), name='index'),
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
-    path('created_by/<int:author_pk>/', created_by, name='created_by'),
-    path('category/<slug:slug>/', category, name='category'),
+    path(
+        'created_by/<int:author_pk>/',
+        CreatedByListView.as_view(),
+        name='created_by',
+    ),
+    path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
     path('tag/<slug:slug>/', tag, name='tag'),
     path('search/', search, name='search'),
 ]
